6284464f2294ee944dd6badb2ec41c41dd176590
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-05-18 16:58:32 +0000

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   get 'home/index'
   get 'home/contact'
   root 'home#about'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:test_user) { create(:user) }
+
+  describe "attributes" do
+    it "has name, email, and password attributes" do
+      expect(test_user).to have_attributes(name: test_user.name, email: test_user.email, password: test_user.password)
+    end
+  end
+
+end
